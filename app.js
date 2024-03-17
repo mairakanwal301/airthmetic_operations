2b51753c441eccd7d72282906a1c4d5e4a9d2440
mairarafiq301 <mairarafiq301@gmail.com>
2024-03-16 17:11:52 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+"use strict";
+// ADDITION OPERATOR:
+let myMango = 8;
+let myFriendMango = 4;
+let totalMangoes = myMango + myFriendMango;
+console.log(totalMangoes);
+//SUBTRACTION OPERATOR:
+let birds = 15;
+let birds_have_fly = 9;
+let birds_left = birds - birds_have_fly;
+console.log(birds_left);
+// MULTIPLICATION OPERATOR:
+let apples_in_each_basket = 4;
+let total_baskets = 6;
+let total_apples = apples_in_each_basket * total_baskets;
+console.log(total_apples);
+// DEVISION OPERATOR:
+let pens = 15;
+let pen_user = 5;
+let each_user_have = pens / pen_user;
+console.log(each_user_have);
+//EXPONENT:
+let layerOfCheese = 5;
+let packets = 10;
+let totalLayers = packets ** layerOfCheese;
+console.log(totalLayers);
+//MODULUS:
+let pencils = 15;
+let pencil_users = 6;
+let reminder = pens / pen_user;
+console.log(reminder);
---
+++ app.ts
@@ -0,0 +1,36 @@
+// ADDITION OPERATOR:
+let myMango: number = 8 ;
+let myFriendMango: number = 4
+let totalMangoes: number = myMango + myFriendMango ;
+console.log(totalMangoes) ;
+
+//SUBTRACTION OPERATOR:
+let birds:number = 15
+let birds_have_fly:  number = 9;
+let birds_left: number = birds - birds_have_fly ;
+console.log(birds_left) ;
+
+// MULTIPLICATION OPERATOR:
+let apples_in_each_basket: number = 4;
+let total_baskets:number = 6 ;
+let total_apples: number = apples_in_each_basket * total_baskets ;
+console.log(total_apples) ;
+
+
+// DEVISION OPERATOR:
+let pens :number = 15
+let pen_user: number = 5
+let each_user_have: number = pens / pen_user ;
+console.log(each_user_have) ;
+
+//EXPONENT:
+let layerOfCheese:number = 5 ;
+let packets:number = 10 ;
+let totalLayers:number = packets ** layerOfCheese ;
+console.log(totalLayers)
+
+//MODULUS:
+let pencils:number = 15
+let pencil_users: number = 6
+let reminder: number = pens / pen_user ;
+console.log(reminder) ;
